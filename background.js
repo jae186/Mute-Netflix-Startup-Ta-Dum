1bbe72a950fda8af844032992a427fdfe6ba18b3
Jayati Gupta <jae186@gmail.com>
2021-07-30 19:08:19 +0530

    toggle button bug squashed

--- background.js
+++ background.js
@@ -1,10 +1,26 @@
 
-var theStatus = true; 
+var theStatus;
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request == true)
+            theStatus = true;
+        else
+            theStatus = false;
+        console.log("background.js got a message")
+        console.log(request);
+        console.log("status is " + theStatus);
+        sendResponse("bar");
+    }
+);
+
 chrome.storage.local.get('enabled', data => {
     if (data.enabled) {
        theStatus = true;
+       console.log(theStatus);
     } else {
         theStatus = false;
+        console.log(theStatus);
     }
 });
 
@@ -23,7 +39,7 @@ function unmuter(tabId){
                 if(tab.url.indexOf("netflix.com/watch/") != -1)  {
                     muter(tabid);
                     console.log("muted")
-                    setTimeout(function(){ unmuter(tabid) }, 10000);
+                    setTimeout(function(){ unmuter(tabid) }, 12000);
                     console.log("unmuted")
                 }
         }}                                                              }
--- popup.js
+++ popup.js
@@ -3,12 +3,20 @@ var myButton = document.getElementById('toggle');
 
 chrome.storage.local.get('enabled', data => {
     enabled = !!data.enabled;
-    myButton.textContent = enabled ? 'Disable' : 'Enable';
+    myButton.textContent = enabled ? 'Currently On' : 'Currently Off';
 });
 
 myButton.onclick = () => {
     enabled = !enabled;
-    myButton.textContent = enabled ? 'Disable' : 'Enable';
+    myButton.textContent = enabled ? 'Currently On' : 'Currently Off';
     chrome.storage.local.set({enabled:enabled});
+    console.log("it is" + enabled);
+
+    chrome.runtime.sendMessage(
+        enabled,
+        function (response) {
+            console.log(response);
+        }
+    );
 };
 
