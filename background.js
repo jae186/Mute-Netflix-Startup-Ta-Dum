c9d31d2fcecc853bbf32165670084340482a888f
Jayati Gupta <jae186@gmail.com>
2021-07-30 18:22:59 +0530

    added toggle + refined html

--- background.js
+++ background.js
@@ -1,4 +1,12 @@
 
+var theStatus = true; 
+chrome.storage.local.get('enabled', data => {
+    if (data.enabled) {
+       theStatus = true;
+    } else {
+        theStatus = false;
+    }
+});
 
 function muter(tabId){
           chrome.tabs.update(tabId, {muted: true});
@@ -8,50 +16,17 @@ function unmuter(tabId){
           chrome.tabs.update(tabId, {muted: false});
         }
 
-
-    
     chrome.tabs.onUpdated.addListener(function(tabid, changeinfo, tab) {
-        
+        //makes sure the listener is only fired once
+        if(theStatus){
             if (changeinfo.status == "complete") {
-                if(tab.url.indexOf("netflix.com/watch/") != -1){
+                if(tab.url.indexOf("netflix.com/watch/") != -1)  {
                     muter(tabid);
+                    console.log("muted")
                     setTimeout(function(){ unmuter(tabid) }, 10000);
-                    console.log("hey")
+                    console.log("unmuted")
                 }
-        }                                                              }
+        }}                                                              }
        );
 
-
-
-
-
-/*
-chrome.tabs.onUpdated.addListener(function(id, info, tab){
-    
-        if (tab.status !== "complete"){
-            return;
-    }
-        if(tab.url.indexOf("netflix") != -1){
-            muter(id);
-            setTimeout(function(){ unmuter(id) }, 10000);
-            console.log("hey")
-    }
-})
-
-
-if (d.getTime() >= (startTime + 7000)){
-    chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
-        const { audible, mutedInfo } = change;
-        if (audible || mutedInfo && !mutedInfo.muted) {
-          chrome.tabs.update(tabId, {muted: true});
-        }
-      });}
-
-function toggleMute(tabId) {
-    chrome.tabs.get(tabId, async (tab) => {
-      let muted = !tab.mutedInfo.muted;
-      await chrome.tabs.update(tabId, { muted });
-      console.log(`Tab ${tab.id} is ${ muted ? 'muted' : 'unmuted' }`);
-    });
-  }
- */
+   
---
+++ popup.js
@@ -0,0 +1,14 @@
+var enabled = true;
+var myButton = document.getElementById('toggle');
+
+chrome.storage.local.get('enabled', data => {
+    enabled = !!data.enabled;
+    myButton.textContent = enabled ? 'Disable' : 'Enable';
+});
+
+myButton.onclick = () => {
+    enabled = !enabled;
+    myButton.textContent = enabled ? 'Disable' : 'Enable';
+    chrome.storage.local.set({enabled:enabled});
+};
+
